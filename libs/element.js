d8a8b37c53981e4c97e6c55c552a7d6ec68952d3
Milos Zivadinovic <mzdv@outlook.com>
2014-11-24 03:28:00 +0100

    Every day you learn something new. Started integrations

--- libs/element.js
+++ libs/element.js
@@ -1,3 +1,21 @@
 /**
  * Created by praetoriaen on 11/10/14.
  */
+//var elementi = [];
+
+var Element = {                         //refactoring inbound
+
+    newElement: function(callsign, path){
+        this.callsign = callsign;
+        this.path = path;
+    },
+
+    createVault: function() {
+        console.log("To be implemented");
+        return 0;
+    }
+};
+
+//elementi.push(Element);               what to do with this?
+
+module.exports = Element;
--- test/element.js
+++ test/element.js
@@ -6,18 +6,20 @@ var Element = require("../libs/element");
 describe("Element", function() {
     describe("#newElement(String callsign, String path)", function() {
         it("Should create a new Element with the callsign and path", function() {
+
             var callsign = "a";
             var path = "a";
 
-            var testElement = new Element(callsign, path);
+            var testElement = new Element.newElement(callsign, path);
 
-            testElement.getCallsign().should.equal("a");
-            testElement.getPath().should.equal("a");
+            testElement.callsign.should.equal(callsign);
+            testElement.path.should.equal(path);
+
         });
     });
     describe("#createVault()", function() {
         it("Should return 0 on successful elementi.json creation", function() {
-            Element.createVault.should.return(0);
+            Element.createVault().should.be(0);
         })
     })
 });
