a6470a0942d5c0a856e49f2e4066efe0284378cb
Milos Zivadinovic <mzdv@outlook.com>
2014-12-01 12:37:31 +0100

    Refactored gatherer tests and model.

--- test/gatherer.js
+++ test/gatherer.js
@@ -4,44 +4,42 @@
 
 var Gatherer = require("../libs/gatherer");
 var Element = require("../libs/element");
+var Parser = require("../libs/parser");
 
 describe("Gatherer", function() {
-    describe("#gather(Element Element)", function() {
-        it("Should return a continuous stream of data", function() {
-            var element = new Element("a", "a");
+    describe("#gather(Array resources)", function() {
+        it("Should return a continuous stream of data from resourceList", function() {
             var testGatherer = new Gatherer();
+            var parser = new Parser("elementum.json");
+            parser.parse();
 
-            testGatherer.gather(element).should.be.type(Object);
+            testGatherer.gather(parser.getResourceList()).should.be.type(Object);
         });
     });
-    describe("#testElementi(Array elementi)", function() {
+    describe("#testElementi(Array resources)", function() {
         it("Should return 0 if all elementi are operational", function() {
-            var primus = new Element("a", "a");
-            var secundus = new Element("b", "b");
-            var tertius = new Element("c", "c");
+            var parser = new Parser("elementum.json");
+            parser.parse();
 
             var testGatherer = new Gatherer();
 
-            var arrayOfElementi = [ primus, secundus, tertius ];
-
-            testGatherer.testElementi(arrayOfElementi).should.return(0);
+            testGatherer.testElementi(parser.getParsedData()).should.return(0);
         });
         it("Should return the faulty interface and error code -1 if the interface isn't operational", function() {
-            var primus = new Element("a", "a");
-            var secundus = new Element("b", "b");
-            var tertius = new Element("c", "c");
+            var parser = new Parser("elementum.json");
+            parser.parse();
 
             var testGatherer = new Gatherer();
 
-            var arrayOfElementi = [ primus, secundus, tertius ];
-
-            testGatherer.testElementi(arrayOfElementi).should.be.type(String).and.return(-1);
+            testGatherer.testElementi(parser.getParsedData()).should.be(-1);
         });
         it("Should return 1 if the array of elementi is empty", function() {
-            var arrayOfElementi = [];
+            var parser = new Parser("elementumEmpty.json");
+            parser.parse();
+
             var testGatherer = new Gatherer();
 
-            testGatherer.testElementi(arrayOfElementi).should.return(1);
+            testGatherer.testElementi(parser.getParsedData()).should.return(1);
         });
     });
 });
